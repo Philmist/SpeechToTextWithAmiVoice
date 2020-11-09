998897818d2f0d7db773ef8d9d72d161f40717e1
philmist <philmist.j@gmail.com>
2020-11-09 13:02:58 +0900

    ライセンス条文を追加。

--- SpeechToTextWithAmiVoice/CaptureVoiceFromWasapi.cs
+++ SpeechToTextWithAmiVoice/CaptureVoiceFromWasapi.cs
@@ -1,4 +1,25 @@
-﻿using NAudio.CoreAudioApi;
+﻿/*
+ * CaptureVoiceFromWasapi.cs
+ * 
+ * Copyright 2020 Philmist
+ * 
+ * Copyright 2017 zufall (from: https://github.com/zufall-upon/kikisen-vc)
+ * 
+ * This file is part of SpeechToTextAmiVoice.
+ * SpeechToTextAmiVoice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using NAudio.CoreAudioApi;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 using System;
